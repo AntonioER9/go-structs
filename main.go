aa3210af755ba58b5cd395c2f63dad073899978c
Antonio Espinoza <a0e0bhj@walmart.com>
2025-03-03 18:08:45 -0300

    refactor main function to initialize person struct fields separately and use Printf for output

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ type person struct {
 }
 
 func main() {
-	antonio := person{firstName: "Antonio", lastName: "Espinoza"}
-	fmt.Println(antonio)
+	// antonio := person{firstName: "Antonio", lastName: "Espinoza"}
+	// fmt.Println(antonio)
+	var antonio person
+	antonio.firstName = "Antonio"
+	antonio.lastName = "Espinoza"
+	fmt.Printf("%+v", antonio)
 }
