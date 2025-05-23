eefd48bbeddb10ade44577c3172adff7fed9a885
Antonio Espinoza <a0e0bhj@walmart.com>
2025-05-23 10:13:37 -0400

    refactor: reorganize person struct and clean up main function

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// antonio := person{firstName: "Antonio", lastName: "Espinoza"}
 	// fmt.Println(antonio)
-	var antonio person
-	antonio.firstName = "Antonio"
-	antonio.lastName = "Espinoza"
-	fmt.Printf("%+v", antonio)
+	// var antonio person
+	// antonio.firstName = "Antonio"
+	// antonio.lastName = "Espinoza"
+	// fmt.Printf("%+v", antonio)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 12345,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
+
 }
